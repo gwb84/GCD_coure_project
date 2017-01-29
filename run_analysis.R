25841326f0cd0fc04292b1b37475e0a9437587ad
gwb84 <gregory.bartram@gmail.com>
2017-01-28 21:37:09 -0500

    wrote output to txt

--- run_analysis.R
+++ run_analysis.R
@@ -12,7 +12,8 @@
 # CodeBook.md describes the dataset and any transformations that have been performed on it
 
 # load feature labels as strings
-feature_labels = read.table('/Users/gwb/Rworking/UCI_HAR_Dataset/features.txt',stringsAsFactors = F)
+# feature_labels = read.table('/Users/gwb/Rworking/UCI_HAR_Dataset/features.txt',stringsAsFactors = F)
+feature_labels = read.table('features.txt',stringsAsFactors = F)
 feature_labels = feature_labels[,2] # only need the second column
 
 # make the feature labels more readable (OBJECTIVE 4)
@@ -25,14 +26,20 @@ feature_labels = gsub('mad','median_absolute_deviation',feature_labels)
 feature_labels = gsub('maxInds','max_freq_inds',feature_labels)
 
 # load training set with feature_labels as column names (OBJECTIVE 4)
-X_training_set = read.table('/Users/gwb/Rworking/UCI_HAR_Dataset/train/X_train.txt',col.names=feature_labels)
+# X_training_set = read.table('/Users/gwb/Rworking/UCI_HAR_Dataset/train/X_train.txt',col.names=feature_labels)
+X_training_set = read.table('X_train.txt',col.names=feature_labels)
+
 # repeat for test set
-X_test_set = read.table('/Users/gwb/Rworking/UCI_HAR_Dataset/test/X_test.txt',col.names=feature_labels)
+# X_test_set = read.table('/Users/gwb/Rworking/UCI_HAR_Dataset/test/X_test.txt',col.names=feature_labels)
+X_test_set = read.table('X_test.txt',col.names=feature_labels)
 
 # load the training set labels, will be integers in [1,5]
-y_training_labels = read.table('/Users/gwb/Rworking/UCI_HAR_Dataset/train/y_train.txt',col.names=c('activity'))
+# y_training_labels = read.table('/Users/gwb/Rworking/UCI_HAR_Dataset/train/y_train.txt',col.names=c('activity'))
+y_training_labels = read.table('y_train.txt',col.names=c('activity'))
+
 # repeat for test set
-y_test_labels = read.table('/Users/gwb/Rworking/UCI_HAR_Dataset/test/y_test.txt',col.names=c('activity'))
+# y_test_labels = read.table('/Users/gwb/Rworking/UCI_HAR_Dataset/test/y_test.txt',col.names=c('activity'))
+y_test_labels = read.table('y_test.txt',col.names=c('activity'))
 
 # OBJECTIVE 1: COMBINE TRAINING AND TEST DATASETS
 X_set = rbind(X_training_set,X_test_set)
@@ -53,7 +60,9 @@ X_set = cbind(X_set_means,X_set_stds)
 
 
 # load the activity label strings 
-activity_labels = read.table('/Users/gwb/Rworking/UCI_HAR_Dataset/activity_labels.txt')
+#activity_labels = read.table('/Users/gwb/Rworking/UCI_HAR_Dataset/activity_labels.txt')
+activity_labels = read.table('activity_labels.txt')
+
 activity_labels = as.character(activity_labels[,2]) #only need the second column
 
 # OBJECTIVE 3 Name the activities in the data set
@@ -76,8 +85,9 @@ for (i in 1:length(activity_labels)){
 tidy_set = as.data.frame(tidy_set,row.names=activity_labels)
 colnames(tidy_set) = X_set_names
 
-write.csv(tidy_set, file = '/Users/gwb/Rworking/GCD_course_project/X_tidy.csv')
-
+#write.csv(tidy_set, file = '/Users/gwb/Rworking/GCD_course_project/X_tidy.csv')
+#write.table(tidy_set, file = '/Users/gwb/Rworking/GCD_course_project/X_tidy.txt',row.names = F)
+write.table(tidy_set, file = 'X_tidy.txt',row.names = F)
 
 
 
